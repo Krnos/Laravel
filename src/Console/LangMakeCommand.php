b9f5c693c441e4077ad6bee1ad83bf4ac34fd139
Ivan Sotelo <isotelo@controlla.com.mx>
2022-04-18 23:21:11 -0600

    fix: snake case

--- src/Console/LangMakeCommand.php
+++ src/Console/LangMakeCommand.php
@@ -93,8 +93,8 @@ class LangMakeCommand extends GeneratorCommand
      */
     protected function getPath($name)
     {
-        $name = Str::replaceFirst($this->rootNamespace(), '', Str::plural(Str::snake($name)));
+        $name = Str::replaceFirst($this->rootNamespace(), '', Str::plural($name));
 
-        return $this->laravel['path'].'/'.str_replace('\\', '/', $name).'.php';
+        return $this->laravel['path'].'/'.str_replace('\\', '/', Str::snake($name)).'.php';
     }
 }
